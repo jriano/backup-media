7bccd0f8799994db9b915ac0bc60f6523afdce2b
Juan Riano <juan.riano@gmail.com>
2018-03-27 08:20:27 -0400

    Script is working, but cannot handle empty strings in file name

--- backup_your_pictures.sh
+++ backup_your_pictures.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Backup your pictures # By Juan C. Riano
+# Backup your pictures
 
-# This script backups your JPG or JPEG pictures organizing them into folders by year and month.
+# By Juan C. Riano
+
+# This script backs up your JPG or JPEG pictures organizing them into folders by year and month.
 # Folders are created only if they do not already exist.
 
-# Dependencies: grep, awk, exif, jq.
+# Dependencies: grep, awk, rsync, exif, jq.
 
 
 # Get directories from dirs.json
@@ -15,36 +17,37 @@
 PICSSOURCE=$( cat dirs.json | jq -r '.source' )
 PICSDESTINATION=$( cat dirs.json | jq -r '.destination' )
 
+BLACKWHOLE=$( which exif )
+if ! [ "$?" -eq "0"  ]; then
+	echo "exif is missing, please install."
+	exit 1
+fi
+BLACKWHOLE=$( which jq )
+if ! [ "$?" -eq "0"  ]; then
+	echo 'jq is missing, please install.'
+	exit 1
+fi
+BLACKWHOLE=$( which rsync )
+if ! [ "$?" -eq "0"  ]; then
+	echo 'rsync is missing, please install.'
+	exit 1
+fi
 if ! [ -f "./dirs.json" ]; then
 	echo "json configuration file is missing."
 	exit 1
 fi
-
-if ! [ -d "$PICSSOURCE" ]; then
-    echo "Picture source directory does not exist."
+if [ ! -d "$PICSSOURCE" ] || [ ! -w "$PICSSOURCE" ]; then
+    echo "Picture source directory does not exist or is not writable."
+    exit 1
+fi
+if [ ! -d "$PICSDESTINATION" ] || [ ! -w "$PICSDESTINATION" ]; then
+    echo "Picture destination directory does not exist or is not writable."
     exit 1
 fi
 
-if ! [ -d "$PICSDESTINATION" ]; then
-    echo "Picture destination directory does not exist."
-    exit 1
-fi
-
-if [ -z $( which exif ) ]; then
-	echo "exif is missing, please install."
-	exit 1
-fi
-
-if [ -z $( which jq ) ]; then
-	echo 'jq is missing, please install.'
-	exit 1
-fi
-
-
-for file in $( find $PICSSOURCE -regextype posix-awk -iregex ".*\.(jpeg|jpg)$" )
+for file in $( find "$PICSSOURCE" -regextype posix-awk -iregex ".*\.(jpeg|jpg)$" )
 do
-	echo "Backing up $file"
-	WHOLESTR=$( exif $file | grep 'Date and Time' | grep 'Origi' )
+	WHOLESTR=$( exif "$file" | grep 'Date and Time' | grep 'Origi' )
 	THEDATE=$( echo "$WHOLESTR" | awk '{split($0,arr,"|"); print arr[2]}' )
 	THEYEAR=$( echo "$THEDATE" | awk '{split($0,arr,":"); print arr[1]}' )
 	THEMONTH=$( echo "$THEDATE" | awk '{split($0,arr,":"); print arr[2]}' )
@@ -56,9 +59,22 @@ do
 		THEFOLDER="$PICSDESTINATION/$THEYEAR-$THEMONTH"	# Folder in format yyyy-mm
 	fi
 
+	echo "Source:      $file"
+	echo "Destination: $THEFOLDER"
+
 	# Create folder, ignore if folder already exists, and copy files
 	$( mkdir -p "$THEFOLDER")
-	mv "$file" "$THEFOLDER"
+	#mv "$file" "$THEFOLDER"
 	#cp --backup=numbered  "$file" "$THEFOLDER"
+	rsync -hz "$file" "$THEFOLDER"
+	if [ "$?" -eq "0" ]; then
+		rm -rf "$file"
+		echo "$file removed from source"
+		echo ""
+	else
+		echo "Failed to backup $file"
+		echo "Aborting copying, please check folder permissions."
+		exit 1
+	fi
 done
 echo "Completed backing up pictures."
